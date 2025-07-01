301497920af2fe94c434c3a3c672cbb2b8771826
Grace <grace6424@icloud.com>
2025-07-01 13:03:21 +0200

    ✅ Bonus Aufgabe

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,15 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
 
-class GoldScreen extends StatelessWidget {
+class GoldScreen extends StatefulWidget {
   const GoldScreen({super.key});
+
+  @override
+  State<GoldScreen> createState() => _GoldScreenState();
+}
+
+class _GoldScreenState extends State<GoldScreen> {
+  double? goldPrice;
+  StreamSubscription<double>? goldPriceSubscription;
+  Stream<double> goldPriceStream = getGoldPriceStream();
+
+  @override
+  void initState() {
+    super.initState();
+    goldPriceSubscription = goldPriceStream.listen(
+      (event) {
+        setState(() {
+          goldPrice = event;
+        });
+      },
+      onError: (error) {
+        debugPrint('Error fetching gold price: $error');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Stream<double> goldPriceStream = getGoldPriceStream();
-    double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -24,32 +48,28 @@ class GoldScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               SizedBox(height: 20),
-              StreamBuilder<double>(
-                stream: goldPriceStream,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    goldPrice = snapshot.data!;
-                  } else if (snapshot.hasError) {
-                    return Text(
-                      'Error retrieving gold price: ${snapshot.error}',
-                    );
-                  }
-                  return Text(
-                    NumberFormat.simpleCurrency(
-                      locale: 'de_DE',
-                    ).format(
-                      goldPrice,
-                    ),
-                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  );
-                },
+              Text(
+                goldPrice != null
+                    ? NumberFormat.simpleCurrency(
+                        locale: 'de_DE',
+                      ).format(
+                        goldPrice,
+                      )
+                    : 'Loading..',
+                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    goldPriceSubscription?.cancel();
+    super.dispose();
   }
 }
