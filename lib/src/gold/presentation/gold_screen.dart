5d3a4339a0ce33a5fa1d4d3eb65d981bd8859bbe
Grace <grace6424@icloud.com>
2025-07-01 12:21:25 +0200

    ✅ Aufgabe 1

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
 
@@ -6,8 +7,7 @@ class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
+    Stream<double> goldPriceStream = getGoldPriceStream();
     double goldPrice = 69.22;
 
     return SafeArea(
@@ -24,13 +24,27 @@ class GoldScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              StreamBuilder<double>(
+                stream: goldPriceStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    goldPrice = snapshot.data!;
+                  } else if (snapshot.hasError) {
+                    return Text(
+                      'Error retrieving gold price: ${snapshot.error}',
+                    );
+                  }
+                  return Text(
+                    NumberFormat.simpleCurrency(
+                      locale: 'de_DE',
+                    ).format(
+                      snapshot.data != null ? snapshot.data! : goldPrice,
+                    ),
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  );
+                },
               ),
             ],
           ),
